1169af6470f7f8017406161c1d2b8db5994d5776
SirImperivm <riccardo.chimento24@gmail.com>
2026-01-08 23:53:49 +0100

    feat(command): add comprehensive documentation for ExecutorType enum and its values

--- src/main/java/me/sirimperivm/commandsAPI/command/model/ExecutorType.java
+++ src/main/java/me/sirimperivm/commandsAPI/command/model/ExecutorType.java
@@ -1,5 +1,18 @@
 package me.sirimperivm.commandsAPI.command.model;
 
+/**
+ * Defines the type of executor that can run a command.
+ *
+ * This enumeration helps to specify the source context from which a command
+ * can be executed. It ensures proper handling and validation of commands
+ * based on their executor type.
+ *
+ * The possible executor types are:
+ *
+ * - PLAYER: Indicates that the command must be executed by a player in-game.
+ * - CONSOLE: Indicates that the command must be executed from the server console.
+ * - BOTH: Indicates that the command can be executed by both the player and the console.
+ */
 public enum ExecutorType {
 
     PLAYER,
