e929075392fdad5cbfec6a5c803c89d76421f29c
SirImperivm <riccardo.chimento24@gmail.com>
2026-01-08 23:55:54 +0100

    feat(command): add documentation for createRegistry method in CommandsAPI

--- src/main/java/me/sirimperivm/commandsAPI/CommandsAPI.java
+++ src/main/java/me/sirimperivm/commandsAPI/CommandsAPI.java
@@ -5,6 +5,12 @@ import org.bukkit.plugin.Plugin;
 
 public final class CommandsAPI {
 
+    /**
+     * Creates and returns a new instance of {@code CommandRegistry} for the specified plugin.
+     *
+     * @param plugin the plugin instance for which the command registry will be created
+     * @return a new {@code CommandRegistry} instance associated with the provided plugin
+     */
     public static CommandRegistry createRegistry(Plugin plugin) {
         return new CommandRegistry(plugin);
     }
